a67b60e09a4e85043d3a2edebaba476659772d7a
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-02-08 12:11:04 +0200

    Message Commencation Passing

--- Threads/app.js
+++ Threads/app.js
@@ -1,16 +1,34 @@
-const {Worker} = require ("worker_threads");
+const {Worker,MessageChannel} = require ("worker_threads");
 
-const a = 400
+// const a = 400
 
-for (let i = 0; i <16; i++) {
-  const thread = new Worker("./calc.js")
-}
+// for (let i = 0; i <16; i++) {
+//   const thread = new Worker("./calc.js")
+// }
 
-// const thread1 = new Worker("./calc.js")
-// const thread2 = new Worker("./calc.js")
+// // const thread1 = new Worker("./calc.js")
+// // const thread2 = new Worker("./calc.js")
 
-// setTimeout(() => {
-//   const thread3 = new Worker("./calc.js")
-// }, 5000)
+// // setTimeout(() => {
+// //   const thread3 = new Worker("./calc.js")
+// // }, 5000)
 
-console.log(a)
+// console.log(a)
+
+const channel =new MessageChannel()
+
+const port1 = channel.port1
+
+const port2 = channel.port2
+
+port1.postMessage({name:"Ali"})
+port2.postMessage({name:"Alaa"})
+
+/port1.on("message",(msg) => {
+  console.log(`Message received from port 1:`,msg)
+})
+port2.on("message", (msg) => {
+  console.log(`Message received from port 2`,msg)
+})
+
+//console.log(channel)
