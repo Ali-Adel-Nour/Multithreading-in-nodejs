a46c652c8c0d0083b7759c90b2820a477d2d3611
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-02-08 13:58:30 +0200

    eaiser way to implment

--- Threads/app.js
+++ Threads/app.js
@@ -36,8 +36,35 @@ const {Worker,MessageChannel} = require ("worker_threads");
 
 /* Communication between 2 workers threads   */
 
-const {port1, port2} = new MessageChannel()
+// const {port1, port2} = new MessageChannel()
 
-const thread1 = new Worker("./calc2.js",{workerData:{port:port1},transferList:[port1]})
-const thread2 = new Worker("./calc2.js",{workerData:{port:port2},transferList:[port2]})
+// const thread1 = new Worker("./calc2.js",{workerData:{port:port1},transferList:[port1]})
 
+// const thread2 = new Worker("./calc2.js",{workerData:{port2},transferList:[port2]})
+
+
+/* Communication between main thread and 2 workers threads */
+const channel1 = new MessageChannel()
+const channel2 = new MessageChannel()
+
+const thread1 = new Worker("./calc2.js",{workerData:{port:channel1.port2},transferList:[channel1.port2]})
+const thread2 = new Worker("./calc2.js",{workerData:{port:channel2.port1},transferList:[channel2.port1]})
+
+
+channel1.port1.on("message", (msg) => {
+  console.log(`Message received from main thread:`,msg)
+})
+
+channel2.port2.on("message", (msg) => {
+  console.log(`Message received from thread 1:`,msg)
+})
+
+
+// /* This easier but with much less contorol */
+// const thread1 = new Worker("./calc2.js")
+
+// thread1.on("message", (msg) => {
+//   console.log("Message received from main thread:",msg)
+// })
+
+// thread1.postMessage({name:"Ali"})
