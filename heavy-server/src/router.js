c7bc4b01e57c6440c4c75a24e247b2997aa9ceaf
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-03-23 14:34:32 +0200

    adding prime-generator

--- heavy-server/src/router.js
+++ heavy-server/src/router.js
@@ -1,5 +1,7 @@
 // Controllers
 const User = require("./controllers/user");
+const generatePrimes = require("../lib/prime-genrator.js");
+const {performance } = require("perf_hooks");
 
 module.exports = (server) => {
   // ------------------------------------------------ //
@@ -23,9 +25,20 @@ module.exports = (server) => {
   // ------------------------------------------------ //
 
   server.route("get", "/api/primes", (req, res) => {
+    const count = Number(req.params.get("count"));
+   let startingNumber = BigInt(req.params.get("start"));
+    const start = performance.now()
+
+    if(startingNumber < BigInt(Number.MAX_SAFE_INTEGER)){
+      startingNumber = Number(startingNumber);
+
+    }
+   const primes = generatePrimes(count, startingNumber,{format:true})
+
     res.json({
-      primes: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29],
-      time: 0.21,
+      primes,
+      time: ((performance.now() - start) / 1000).toFixed(2),
+
     });
   });
 };
